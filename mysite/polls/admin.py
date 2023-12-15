033124ba238e008b3b5d74fb290edc07658695c8
unknown <mosielite@gmail.com>
2023-12-16 00:04:08 +0330

    add project

---
+++ mysite/polls/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+
+from .models import Choice, Question
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+    list_display = ["question_text", "pub_date"]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+
+
+admin.site.register(Question, QuestionAdmin)
